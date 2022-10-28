980f585fbab79b6c33954030d57d0436905cb62f
zhaogongwei <zhaogongwei@qizuang.com>
2022-10-28 17:35:16 +0800

    js 作用域链

--- javascript-05-作用域链.js
+++ javascript-05-作用域链.js
@@ -96,3 +96,136 @@
     */
 
 }
+
+
+{
+    function a() {
+        var aaa = 123;
+        function b(){
+           console.log(aaa); 
+           aaa=234;
+     };
+        b();
+        console.dir(aaa);
+     };
+     a();
+
+     /* 
+        1、a函数被创建，保存作用域链到内部属性[[scope]]
+
+        a.[[scope]] = [
+            globalContext.VO
+        ]
+
+        2、要执行函数a了，创建函数a的执行上下文，并且将执行上下文压入执行上下文栈
+
+        ECStack=[
+            aContext,
+            globalContext.VO
+        ]
+
+        3、a函数并不立刻执行，做准备工作，复制函数a的[[scope]]属性创建作用域链
+
+        aContext = {
+            Scope: ascope.[[scope]]
+        }
+
+        4、b函数创建
+
+        b.[[scope]] = [
+            globalConext.VO
+        ]
+        5、要执行b函数了，创建函数b的执行上下文，并且将执行上下文压入执行上下文栈
+
+        ECStack = [
+            bContext,
+            aContext,
+            globalContext
+        ]
+
+        6、创建函数a活动对象，随后初始化函数a活动对象（加入形参、函数声明、变量声明）
+
+        aContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                }
+                aaa: undefined
+            },
+            Scope: ascope.[[scope]]
+        }
+        7、将函数a活动对象压入ascope作用域链顶端
+
+        aContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                },
+                aaa: undefined
+            },
+            Scope: [AO, [[Scope]]]
+        }
+
+        8、准备工作做完，开始执行函数a，随着函数a的执行，修改AO的属性值
+
+        aContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                }    
+                aaa: 123
+            },
+            Scope: [AO, [Scope]]
+        }
+
+        9、随着函数a的执行，函数b创建活动对象，随后初始化函数b的活动对象（加入形参、函数声明、变量声明）
+
+        bContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                }
+                // aaa: undefined // 如果有var的话，会有此处
+            },
+            Scope: bscope.[[scope]]
+        }
+
+        10、将函数b活动对象压入bscope作用域链顶端
+
+        bContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                }
+                // aaa: undefined // 如果有var的话，会有此处
+            },
+            Scope: [AO, bscope.[[scope]]]
+        }
+
+        11、准备工作完成，开始执行函数b，随着函数b的执行修改AO属性值
+
+        aContext = {
+            AO: {
+                arguments: {
+                    length: 0
+                }    
+                // aaa: 234; // 如果有var的话，会有此处
+            },
+            Scope: [AO, [Scope]] // 没有var的话，会修改[[Scope]]中父作用域链的值，所以第2个console才是234，否则，只能修改本函数上下AO中的值，改变不吝父作用域链的aaa，父作用域连只能是123了。
+        }
+
+        12、函数b执行完毕，函数上下文从执行上下文栈中弹出
+
+        ECStack = [
+            aContext,
+            globalContext
+        ]
+
+        13、函数a执行完毕，函数上下文从执行上下文栈中弹出
+
+        ECStack = [
+            globalContext
+        ]
+     
+     */
+}
